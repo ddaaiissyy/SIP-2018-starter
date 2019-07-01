321313cb1af3169835678ad5106a0d8c5e72756e
ddaaiissyy <daisy.ibv@gmail.com>
2019-07-01 14:51:30 -0700

    Data Project Week 3

--- U2-Applications/U2.1-Data/data_vis_project_starter.py
+++ U2-Applications/U2.1-Data/data_vis_project_starter.py
@@ -1,7 +1,7 @@
 '''
 In this project, you will visualize the feelings and language used in a set of
 Tweets. This starter code loads the appropriate libraries and the Twitter data you'll
-need!
+need!py
 '''
 
 import json
@@ -13,8 +13,63 @@ tweetFile = open("TwitterData/tweets_small.json", "r")
 tweetData = json.load(tweetFile)
 tweetFile.close()
 
-# Continue your program below! 
+# Continue your program below!
 
-# Textblob sample:
-tb = TextBlob("You are a brilliant computer scientist.")
-print(tb.polarity)
+def Average(lst): 
+    return sum(lst) / len(lst)
+
+tweettext = []
+tweetstring = ''
+for tweet in tweetData:
+    tweetstring += tweet['text']
+print(tweetstring)
+
+
+
+    # blob = TextBlob(tweet["text"])
+    # tweettext.append(blob)
+   
+blob_polarity = []
+for blob in tweetData:
+    b = TextBlob(blob['text'])
+    blob_polarity.append(b.polarity)
+# print(blob_polarity)
+
+blob_subjectivity = []
+for blob in tweetData:
+    b = TextBlob(blob['text'])
+    blob_subjectivity.append(b.subjectivity)
+
+print(Average(blob_polarity))
+print(Average(blob_subjectivity))
+
+tb = TextBlob(tweetstring)
+word_dict = {}
+for word in tb.words:
+    word_dict[word.lower()] = tb.word_counts[word.lower()]
+print(word_dict)
+
+
+#HISTOGRAM
+import matplotlib.pyplot as plt
+
+someList = [0.2, -0.3, -0.4, -1, 1, 0.3, 0.6, 0.2, 0.14, -0.16, -0.18, 0.25]
+plt.hist(someList, bins=[-1, -0.5, 0.0, 0.5, 1])
+plt.xlabel('Values')
+plt.ylabel('Number of Items')
+plt.title('POLARITY')
+plt.axis([-1.1, 1.1, 0, 6])
+plt.grid(True)
+plt.show()
+0.1403954771379014
+
+import matplotlib.pyplot as plt
+
+someList = [0.2, -0.3, -0.4, -1, 1, 0.3, 0.6, 0.2, 0.14, -0.16, -0.18, 0.25]
+plt.hist(someList, bins=[-1, -0.5, 0.0, 0.5, 1])
+plt.xlabel('Values')
+plt.ylabel('Number of Items')
+plt.title('SUBJECTIVITY') 
+plt.axis([-1.1, 1.1, 0, 6])
+plt.grid(True)
+plt.show()
